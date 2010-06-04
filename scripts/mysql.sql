f927302e4b22d63047efc9b2a8deb1977c4f11a5
Argel Arias <levhita@gmail.com>
2010-06-04 14:02:51 -0500

    Started Database Diagram

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,57 +1,132 @@
-CREATE TABLE `Categoria` (
-      `CatID` tinyint(4) NOT NULL auto_increment,
-      `Categoria` varchar(50) NOT NULL default '',
-      PRIMARY KEY  (`CatID`),
-      UNIQUE KEY `Categoria` (`Categoria`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-    INSERT INTO `Categoria` VALUES (1,'Almacenamiento'),(2,'Audio/Sonido'),
-                                   (3,'Cables'),        (4,'Camaras'),
-                                   (5,'Copiadoras'),    (6,'Energia'),
-                                   (7,'FAX'),           (8,'Gabinetes'),
-                                   (9,'Impresoras'),    (10,'Memoria'),
-                                   (11,'Miscelanea'),   (12,'Monitores'),
-                                   (13,'Mouse'),        (14,'Muebles'),
-                                   (15,'Notebooks'),    (16,'PCs'),
-                                   (17,'PDAs'),         (18,'Redes'),
-                                   (19,'Scanners'),     (20,'Tarjetas Madres'),
-                                   (21,'Teclados'),     (22,'Telefonia'),
-                                   (23,'Video'),        (26,'No Computacional');
+CREATE SCHEMA IF NOT EXISTS `tianguiscabal` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `tianguiscabal`;
 
-    CREATE TABLE `Vendedores` (
-      `UserID` int(7) NOT NULL auto_increment,
-      `ApellidoPaterno` varchar(25) NOT NULL default '',
-      `ApellidoMaterno` varchar(25) default '',
-      `Nombres` varchar(30) NOT NULL default '',
-      `Login` varchar(25) NOT NULL default '',
-      `PWD` varchar(50) NOT NULL default '',
-      `Fecha` date NOT NULL default '0000-00-00',
-      `Correo` varchar(75) NOT NULL default '',
-      `MexTelLada` varchar(2) NOT NULL default '0',
-      `MexTelFront` varchar(4) NOT NULL default '0',
-      `MexTelBack` varchar(4) NOT NULL default '0',
-      PRIMARY KEY  (`UserID`),
-      UNIQUE KEY `Login` (`Login`),
-      UNIQUE KEY `Correo` (`Correo`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `tianguiscabal`.`category`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tianguiscabal`.`category` (
+  `category_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`category_id`) )
+ENGINE = InnoDB
+COMMENT = 'Holds the Categories';
 
-    CREATE TABLE `Ventas` (
-      `VentaID` int(7) NOT NULL auto_increment,
-      `UserID` int(7) NOT NULL default '0',
-      `Fecha` date NOT NULL default '0000-00-00',
-      `Articulo` varchar(175) NOT NULL default '',
-      `Cantidad` tinyint(4) NOT NULL default '1',
-      `Descripcion` varchar(250) NOT NULL default '',
-      `Calidad` enum('Excelente','Muy Buena','Buena','Regular','Partes') default 'Muy Buena',
-      `Precio` varchar(10) NOT NULL default '00.00',
-      `LinkFoto` varchar(175) default '',
-      `CompraVenta` enum('Se Vende','Quiero Comprar') default 'Se Vende', `InterCambiar` varchar(12) default '',
-      `CambiarParaQue` varchar(250) default '',
-      `Categoria` varchar(50) NOT NULL default '',
-      PRIMARY KEY  (`VentaID`),
-      KEY `UserID` (`UserID`),
-      KEY `Categoria` (`Categoria`),
-      CONSTRAINT `Ventas_ibfk_2` FOREIGN KEY (`Categoria`) REFERENCES `Categoria` (`Categoria`),
-      CONSTRAINT `Ventas_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Vendedores` (`UserID`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- -----------------------------------------------------
+-- Table `tianguiscabal`.`user`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tianguiscabal`.`user` (
+  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `password` CHAR(40) NOT NULL ,
+  `description` VARCHAR(255) NULL ,
+  `full_name` VARCHAR(99) NOT NULL ,
+  `email` VARCHAR(45) NOT NULL ,
+  `twitter` VARCHAR(45) NULL ,
+  `date` DATE NOT NULL ,
+  `role` ENUM('registered', 'moderator', 'admin') NOT NULL DEFAULT 'registered' ,
+  PRIMARY KEY (`user_id`) )
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `name` ON `tianguiscabal`.`user` (`name` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tianguiscabal`.`product`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tianguiscabal`.`product` (
+  `product_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NOT NULL ,
+  `category_id` INT UNSIGNED NOT NULL ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(45) NOT NULL ,
+  `picture` VARCHAR(99) NULL ,
+  `price` VARCHAR(45) NOT NULL DEFAULT 0 ,
+  `quantity` TINYINT NOT NULL DEFAULT 1 ,
+  `quality` ENUM('excellent', 'very good', 'good', 'average', 'parts') NOT NULL DEFAULT 'good' ,
+  `transaction` ENUM('buy', 'sell', 'exchange') NOT NULL DEFAULT 'sell' ,
+  `exchange_for` VARCHAR(99) NULL ,
+  `date` DATE NOT NULL ,
+  PRIMARY KEY (`product_id`) ,
+  CONSTRAINT `fk_product_user`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `tianguiscabal`.`user` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product_category1`
+    FOREIGN KEY (`category_id` )
+    REFERENCES `tianguiscabal`.`category` (`category_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_product_user` ON `tianguiscabal`.`product` (`user_id` ASC) ;
+
+CREATE INDEX `fk_product_category1` ON `tianguiscabal`.`product` (`category_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tianguiscabal`.`comment`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tianguiscabal`.`comment` (
+  `comment_id` INT NOT NULL ,
+  `user_id` INT UNSIGNED NOT NULL ,
+  `product_id` INT UNSIGNED NOT NULL ,
+  `comment` VARCHAR(255) NOT NULL ,
+  `date` TIMESTAMP NOT NULL ,
+  PRIMARY KEY (`comment_id`) ,
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `tianguiscabal`.`user` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_product1`
+    FOREIGN KEY (`product_id` )
+    REFERENCES `tianguiscabal`.`product` (`product_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_comment_user1` ON `tianguiscabal`.`comment` (`user_id` ASC) ;
+
+CREATE INDEX `fk_comment_product1` ON `tianguiscabal`.`comment` (`product_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tianguiscabal`.`report`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tianguiscabal`.`report` (
+  `report_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NOT NULL ,
+  `description` VARCHAR(45) NOT NULL ,
+  `url` VARCHAR(99) NOT NULL ,
+  `date` TIMESTAMP NOT NULL ,
+  PRIMARY KEY (`report_id`) ,
+  CONSTRAINT `fk_report_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `tianguiscabal`.`user` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_report_user1` ON `tianguiscabal`.`report` (`user_id` ASC) ;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `tianguiscabal`.`user`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `tianguiscabal`;
+INSERT INTO `user` (`user_id`, `name`, `password`, `description`, `full_name`, `email`, `twitter`, `date`, `role`) VALUES (0, 'admin', '39528eed7b20660478bdb72704bcd7e028a1ae04', 'Temporal Admin, you should really change this', 'Temporal Admin', 'admin@example.com', 'example', NOW(), 'admin');
+
+COMMIT;
