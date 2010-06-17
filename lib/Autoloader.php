245a9c687f2d1ac316fd9cfd90457ae505259343
Argel Arias <levhita@gmail.com>
2010-06-17 13:08:09 -0500

    Changes Autoloader name

---
+++ lib/Autoloader.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Include files based on the Instance's ClassName being created
+ *
+ * Gets {@link Controller} from application/controllers/
+ * Gets {@link View}s from application/views/
+ * Gets {@link Model}s from application/models/
+ * Every other class it gets it from application/core/
+ *
+ * For classes that doesn't form part of the core, you must include the file
+ * by hand.
+ *
+ * @package Garson
+ */
+function __autoload($class_name)
+{
+    $class_name = ucwords($class_name);
+    
+    /** Application Specific Classes are inside this directories **/
+    $named_directories = array (
+        'Controller' => 'controllers/',
+        'View' => 'views/',
+        'Model' => 'models/',
+    );
+
+    $is_core = true;
+
+    foreach ( $named_directories AS $name => $directory ) {
+        if ( stristr( $class_name, $name ) && $class_name != $name ) {
+            $path = $directory . $class_name;
+            $is_core = false;
+            break;
+        }
+    }
+
+    /** All other classes are inside the core **/
+    if ( $is_core ) {
+        $path = '../lib/core/' . $class_name;
+    }
+   
+    /** add the application path and the php extension **/
+    require_once APPLICATION_PATH . '/' . $path . '.php';
+}
